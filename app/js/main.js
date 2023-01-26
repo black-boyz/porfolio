13ae164558542f55876031c797569b60acb449c5
Anton <73070947+black-boyz@users.noreply.github.com>
2023-01-26 22:53:52 +0100

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.prices__list').slick({
+        slidesToShow: 3.5,
+        slidesToScroll: 1,
+        infinite:false,
+        arrows: false,
+        
+        
+        
+       
+      });
+
+
+  
+});
